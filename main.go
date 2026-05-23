fd1fbdc413d19c94c9a36d6dd358f249924da652
foom <web@blazinghoops.com>
2026-05-23 13:43:27 -0500

    first tcp connectivity

--- main.go
+++ main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 )
 
 func main() {
 	if len(os.Args) != 3 {
 		fmt.Println("Usage: muddle <host> <port>")
-		fmt.Println("Example: muddle torilmud.com 9999")
 		os.Exit(1)
 	}
 
 	host := os.Args[1]
 	port := os.Args[2]
 
-	fmt.Printf("Muddle connecting to %s:%s...\n", host, port)
+	address := host + ":" + port
+
+	fmt.Printf("Connecting to %s...\n", address)
+
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		fmt.Println("Connection error:", err)
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Connected successfully.")
 }
