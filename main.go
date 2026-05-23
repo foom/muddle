0afbb3ae5fa3eb04bb9b024ccd39e858de7d8c1e
foom <web@blazinghoops.com>
2026-05-23 13:40:53 -0500

    accept address and port args

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Muddle starting...")
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: muddle <host> <port>")
+		fmt.Println("Example: muddle torilmud.com 9999")
+		os.Exit(1)
+	}
+
+	host := os.Args[1]
+	port := os.Args[2]
+
+	fmt.Printf("Muddle connecting to %s:%s...\n", host, port)
 }
